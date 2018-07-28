af55f5fa06c6cec1bd526fabd806cb8154e8a34d
roman731 <roman.2k5@gmail.com>
2018-07-28 15:07:51 -0400

    Added input box which does a match query on input entered

--- src/App.js
+++ src/App.js
@@ -1,54 +1,66 @@
 import React, { Component } from 'react';
-import elasticsearch from 'elasticsearch';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			data: []
+			data: [],
+			searchValue: ''
 		};
-
 	}
 
 	componentWillMount() {
 		console.log('this function works');
-		//Elasticsearch Client Request
-		// const client = new elasticsearch.Client({
-			// host: 'localhost:9200',
-			// log: 'trace'
-		// });
-		// const exists = client.getSource({
-			// index: 'redditnba',
-			// type: 'post',
-			// id: 1
-		// })
-		// .then(result => {
-			// console.log(result);
-		// });
 		
-		//Axios Request
-		//  axios.get('http://localhost:9200/redditnba/post/_search')
-      	// .then(res => {
-		// 	console.log(res);
-      	// })
+		// Axios Request
+		 axios.get('http://localhost:9200/redditnba/post/_search?size=25')
+      	.then(res => {
+			this.setState({
+				data: res.data.hits.hits
+			});
+      	});
 	}
 
 	mainButtonClick() {
 		console.log('main button clicked');
-		this.setState({
-			data: ['anotherone.ca', 'hckrnews.com']
+		const searchQuery = {
+    		query: { 
+				match: { 
+					"title": this.state.searchValue
+				} 
+			}
+  		};
+
+		axios.get('http://localhost:9200/redditnba/post/_search', {params: {
+			source: JSON.stringify(searchQuery),
+			source_content_type: 'application/json'
+		}})
+		.then(res => {
+			// console.log(res);
+			// console.log(res.data.hits.hits);
+			this.setState({
+				data: res.data.hits.hits
+			});
 		});
 	}
+
+	updateSearchValue (evt) {
+		this.setState({
+			searchValue: evt.target.value
+		});
+  	}
 
 	render() {
 		return (
 			<div className="App">
+				<input type="text" value={this.state.searchValue} onChange={evt => this.updateSearchValue(evt)} style={{marginTop: "10px"}}></input>
 				<ul>
 					{
-						this.state.data.map((item, i) => {
-							return (<li key={i}>{item}</li>)
+						this.state.data.map((document, i) => {
+							return (<li key={i}><a href={document._source.url}>{document._source.title}</a></li>)
 						})
 					}
 				</ul>
